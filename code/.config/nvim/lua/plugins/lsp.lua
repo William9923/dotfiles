c1006e8b6bb68452d11aaa47ff97b457ea4fd4b3
William9923 <williamong1400@gmail.com>
2025-09-06 17:32:35 +0700

    fix volar on init type definition

--- code/.config/nvim/lua/plugins/lsp.lua
+++ code/.config/nvim/lua/plugins/lsp.lua
@@ -96,7 +96,33 @@ return {
             },
           },
         },
-        volar = {},
+        volar = {
+          on_init = function(client)
+            client.handlers['tsserver/request'] = function(_, result, context)
+              local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+              if #vtsls_clients == 0 then
+                vim.notify('Could not find `vtsls` lsp client, `volar` would not work without it.', vim.log.levels.ERROR)
+                return
+              end
+              local vtsls_client = vtsls_clients[1]
+
+              local param = unpack(result)
+              local id, command, payload = unpack(param)
+              vtsls_client:exec_cmd({
+                title = 'volar_request_forward',
+                command = 'typescript.tsserverRequest',
+                arguments = {
+                  command,
+                  payload,
+                },
+              }, { bufnr = context.bufnr }, function(_, r)
+                local response = r and r.body
+                local response_data = { { id, response } }
+                client:notify('tsserver/response', response_data)
+              end)
+            end
+          end,
+        },
       },
       gopls = {},
     },
